75e66387f4028023c7a64a8fc60ebe276771f6fa
Maxwell Terpstra <telic.detour+github@gmail.com>
2021-05-22 20:04:54 -0700

    move dom operations to another file
    also remove some redundant variables from merge

---
+++ domOps.js
@@ -0,0 +1,47 @@
+const gameRoot = document.getElementById('root');
+let input = {
+}
+
+
+function addObjectToDOM(obj) {
+	// TODO
+	// build sprite and hold ref in obj.domRef
+}
+
+function removeObjectFromDOM(obj) {
+	// TODO
+}
+
+const mouseToMass = x => x / 50
+
+const clampMass = x => Math.max(Math.min(x, MASS_MAX), MASS_MIN)
+
+gameRoot.addEventListener('mousedown', e => {
+	input.mouseDown = true
+
+	if(e.target && e.target.dataset.type === 'well') {
+		input.target = e.target.dataset.id
+		// input.targetMass = objects[input.target].physics.mass
+		input.targetMass = 50
+		input.mouseStart = [e.clientX, e.clientY]
+		console.log(input.target)
+	}
+})
+
+gameRoot.addEventListener('mousemove', e => {
+	if(input.mouseDown && input.target) {
+		let mouseDelta = [e.clientX - input.mouseStart[0], e.clientY - input.mouseStart[1]]
+		let magnitude = 0
+		if(Math.abs(mouseDelta[0]) > Math.abs(mouseDelta[1])) {
+			magnitude = mouseDelta[0]
+		} else {
+			magnitude = mouseDelta[1] * -1
+		}
+		// objects[target].physics.mass = clampMass(magnitude + input.targetMass)
+		document.getElementById("hud-debug").innerHTML = clampMass(magnitude + input.targetMass)
+	}
+})
+
+gameRoot.addEventListener('mouseup', e => {
+	input = {}
+})
--- game.js
+++ game.js
@@ -1,11 +1,4 @@
-const gameRoot = document.getElementById('root');
-
-let gameStart = null; // Date
 let objects = []; // [{ ship }, { well }, { well }...]
-let paused = false;
-let input = {
-}
-
 
 const game = {
 	score: 0,
@@ -162,16 +155,6 @@ function gameTick() {
 	}
 }
 
-function addObjectToDOM(obj) {
-	// TODO
-	// build sprite and hold ref in obj.domRef
-}
-
-function removeObjectFromDOM(obj) {
-	// TODO
-}
-
-
 // F = ma
 // F = mMG/d^2
 // a = F / m = MG/d^2
@@ -187,37 +170,3 @@ function acceleration(obj1, obj2) {
 	obj1.physics.acceleration[0] += -1 * obj2.physics.mass * xAccel;
 	obj1.physics.acceleration[1] += -1 * obj2.physics.mass * yAccel;
 }
-
-const mouseToMass = x => x / 50
-
-const clampMass = x => Math.max(Math.min(x, MASS_MAX), MASS_MIN)
-
-gameRoot.addEventListener('mousedown', e => {
-	input.mouseDown = true
-
-	if(e.target && e.target.dataset.type === 'well') {
-		input.target = e.target.dataset.id
-		// input.targetMass = objects[input.target].physics.mass
-		input.targetMass = 50
-		input.mouseStart = [e.clientX, e.clientY]
-		console.log(input.target)
-	}
-})
-
-gameRoot.addEventListener('mousemove', e => {
-	if(input.mouseDown && input.target) {
-		let mouseDelta = [e.clientX - input.mouseStart[0], e.clientY - input.mouseStart[1]]
-		let magnitude = 0
-		if(Math.abs(mouseDelta[0]) > Math.abs(mouseDelta[1])) {
-			magnitude = mouseDelta[0]
-		} else {
-			magnitude = mouseDelta[1] * -1
-		}
-		// objects[target].physics.mass = clampMass(magnitude + input.targetMass)
-		document.getElementById("hud-debug").innerHTML = clampMass(magnitude + input.targetMass)
-	}
-})
-
-gameRoot.addEventListener('mouseup', e => {
-	input = {}
-})
